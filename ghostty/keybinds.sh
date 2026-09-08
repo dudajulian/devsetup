3b9165b101f419f34ce1d103dac6d68604304d11
Julian Duda <dudajulian@proton.me>
2026-09-08 14:38:35 +0200

    init

---
+++ ghostty/keybinds.sh
@@ -0,0 +1,5 @@
+
+# free up ctrl+alt+arrow for ghostty
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Page_Up', '<Super>KP_Prior', '<Super><Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Page_Down', '<Super>KP_Next', '<Super><Alt>Right']"
+
---
+++ nvim/setdefault.md
@@ -0,0 +1,11 @@
+Configure update-alternatives:
+'''bash
+sudo update-alternatives --config vim
+'''
+'''bash
+sudo update-alternatives --config vi
+'''
+'''bash
+sudo update-alternatives --config editor
+'''
+
